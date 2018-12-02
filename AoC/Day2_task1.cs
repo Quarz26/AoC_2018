d13faba77716051c149f2fb110074d822cf2ac16
Vid Jurić <vjuric@stringbind.com>
2018-12-02 13:12:08 +0100

    2nd task

---
+++ AoC/Day2_task1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    class Day2_task1
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int twiceCount = 0;
+            int threeCount = 0;
+            int[] counter;
+
+
+            System.IO.StreamReader input = new System.IO.StreamReader(@"C:\Users\Vid\Desktop\inputDay2.txt");
+
+            while ((line = input.ReadLine()) != null)
+            {
+                counter = new int[26];
+                bool notTwo = true;
+                bool notThree = true;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    char letter = line[i];
+                    counter[(int)letter % 97]++;
+                }
+
+                for (int i = 0; i < 26; i++)
+                {
+                    if (counter[i] == 2 && notTwo)
+                    {
+                        notTwo = false;
+                        twiceCount++;
+                    }
+
+                    if (counter[i] == 3 && notThree)
+                    {
+                        notThree = false;
+                        threeCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Broj: " + threeCount * twiceCount);
+        }
+    }
+}
---
+++ AoC/Day2_task2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    class Day2_task2
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Vid\Desktop\inputDay2.txt");
+            Array.Sort(lines);
+
+            //List<string> dict = new List<string>();
+            //foreach (string s in lines)
+            //{
+
+            //    string intersection = String.Concat(s.Intersect(lines));
+            //}
+            bool exit = true;
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                string first = lines[i];
+
+                for(int j = 0; j < lines.Length; j++)
+                {
+                    string second = lines[j];
+                    string intersection = String.Concat(first.Intersect(second));
+
+                    if(intersection.Length == 25)
+                    {
+                        string result = String.Concat(first.Where(c => intersection.Contains(c)));
+                        Console.WriteLine(result);
+                        exit = false;
+                        break;
+                    }
+                }
+
+                if (!exit) break;
+            }
+
+            //dict.Sort();
+
+            //foreach(string s in dict)
+            //{
+
+            //}
+        }
+    }
+}
