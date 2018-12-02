accd08a8c6baa19811a32ba2b138d9fd774d4e64
Vid Jurić <juric.vid26@gmail.com>
2018-12-02 10:50:22 +0100

    Day 1 task

---
+++ AoC/Day1_task1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    class Day1_task1
+    {
+        static void Main(String[] args)
+        {
+            int freq = 0;
+            string line;
+
+            System.IO.StreamReader input = new System.IO.StreamReader(@"E:\Vid\AoC\AoC\AoC\inputDay1.txt");
+
+            while((line = input.ReadLine()) != null)
+            {
+                freq += Int32.Parse(line);
+            }
+
+            Console.WriteLine("Konačna frekvencija: " + freq);        
+            input.Close();
+
+        }
+    }
+}
---
+++ AoC/Day1_task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    class Day1_task2
+    {
+        static void Main(String[] args)
+        {
+            int resFreq = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"E:\Vid\AoC\AoC\AoC\inputDay1.txt");
+
+            List<int> dict = new List<int>();
+            bool br = false;
+
+            dict.Add(0);
+
+            while (!br)
+            {
+                foreach (string s in lines)
+                {
+                    resFreq += Int32.Parse(s);
+
+                    if (dict.Contains(resFreq))
+                    {
+                        Console.WriteLine("Frekvencija koja se prva ponavlja: " + resFreq);
+                        br = true;
+                        break;
+                    }
+
+                    dict.Add(resFreq);
+                }
+            }
+        }
+    }
+}
