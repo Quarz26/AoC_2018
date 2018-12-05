7eb3cfd948f147ee3ad14a6a7758398241092042
Vid Jurić <juric.vid26@gmail.com>
2018-12-05 09:31:28 +0100

    5th task solved

---
+++ AoC/Day5_task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    class Day5_task1
+    {
+        static void Main(string[] args)
+        {
+            string lines = System.IO.File.ReadAllText(@"C:\Users\juric\OneDrive\Desktop\inputDay5.txt");
+
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in lines)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if ((c ^ stack.Peek()) == 32)
+                        stack.Pop();
+                    else
+                        stack.Push(c);
+                }
+            }
+
+            Console.WriteLine("Nakon reakcija u polimeru ostaje: " + stack.Count + " jedinica");
+        }
+    }
+}
---
+++ AoC/Day5_task2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    class Day5_task2
+    {
+        static void Main (string[] args)
+        {
+            string lines = File.ReadAllText(@"C:\Users\juric\OneDrive\Desktop\testInput.txt");
+
+            int min = int.MaxValue; ;
+
+            for(char i = 'a'; i <= 'z'; i++)
+            {
+                string tempLines = lines.Replace(i.ToString(), "").Replace(char.ToUpper(i).ToString(), "");
+
+                Stack<char> stack = new Stack<char>();
+                foreach (char c in tempLines)
+                {
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        if ((c ^ stack.Peek()) == 32)
+                            stack.Pop();
+                        else
+                            stack.Push(c);
+                    }
+                }
+
+                if(stack.Count < min)
+                {
+                    min = stack.Count;
+                }
+            }
+
+            Console.WriteLine("Najkraći polimer je veličine: " + min);
+
+        }
+    }
+}
